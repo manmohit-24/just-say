Manmohit-Singh24/mystery-message

feat(error): add error handling to @repo/worker

apps/worker/src/emailWorker.ts
@@ -1,9 +1,11 @@
-import { EMAIL_QUEUE_NAME, parseEmailJobData } from "@repo/jobs/email";
-import { Job, Worker } from "bullmq";
+import { EMAIL_QUEUE_NAME } from "@repo/jobs/email";
+import { Job, UnrecoverableError, Worker } from "bullmq";
 import { sendEmail } from "./mail/mail.js";
 import { env } from "./config/env.js";
 import { logger } from "./shared/logger.js";
 import { JobLogger } from "./shared/jobLogger.js";
+import { NonRetryableError, RetryableError } from "@/shared/errors/errors.js";
+import { validateEmailJobData } from "@/mail/validateEmailJobData.js";
 
 const emailJob = async (job: Job) => {
   const jobLogger = new JobLogger({
@@ -13,22 +15,30 @@ const emailJob = async (job: Job) => {
   });
 
   job.jobLogger = jobLogger;
+  try {
+    const data = validateEmailJobData(job.data);
 
-  const response = parseEmailJobData(job.data);
+    jobLogger.options.info = {
+      to: data.to,
+      template: data.template,
+    };
 
-  if (!response.success) {
-    logger.error(response.error);
-    return;
+    await sendEmail(data);
+  } catch (error) {
+    if (error instanceof RetryableError) throw error;
+
+    if (error instanceof NonRetryableError) {
+      const unrecoverable = new UnrecoverableError(error.message);
+      unrecoverable.cause = error;
+      throw unrecoverable;
+    }
+
+    // Unknown/unexpected error:
+    // fail permanently instead of retrying.
+    const unrecoverable = new UnrecoverableError("Unexpected error while processing email job.");
+    unrecoverable.cause = error;
+    throw unrecoverable;
   }
-
-  const data = response.data;
-
-  jobLogger.options.info = {
-    to: data.to,
-    template: data.template,
-  };
-
-  await sendEmail(data);
 };
 
 const emailWorker = new Worker(EMAIL_QUEUE_NAME, emailJob, {
@@ -42,8 +52,14 @@ emailWorker.on("completed", (job) => {
 });
 
 emailWorker.on("failed", (job, err) => {
-  if (!job) logger.error({ err }, "Job failed but no job instance was available");
-  else job.jobLogger.fail(err, { attempt: job.attemptsMade });
+  if (!job) {
+    logger.error({ err }, "Job failed but no job instance was available");
+    return;
+  }
+
+  job.jobLogger.fail(err, {
+    attempt: job.attemptsMade,
+  });
 });
 
 export { emailWorker };

packages/jobs/src/email/templateData.ts
@@ -1,4 +1,4 @@
-import z from "zod";
+import z, { ZodError } from "zod";
 import { emailTemplates, type EmailTemplateName } from "./templates.js";
 
 const emailTemplateDataSchemas: Record<EmailTemplateName, z._ZodType> = {
@@ -77,48 +77,23 @@ type EmailJobData<T extends EmailTemplateName> = {
   data: EmailTemplateData[T];
 };
 
-type EmailJobParseResult =
-  | {
-      success: true;
-      data: EmailJobData<EmailTemplateName>;
-    }
-  | {
-      success: false;
-      error: z.ZodError;
-    };
-
-function parseEmailJobData(input: unknown): EmailJobParseResult {
-  const baseResult = z
+function parseEmailJobData(input: unknown): EmailJobData<EmailTemplateName> {
+  const parsedData = z
     .object({
       to: z.email(),
-      template: z.enum(Object.keys(emailTemplates) as [EmailTemplateName, ...EmailTemplateName[]]),
-      data: z.unknown(),
+      template: z.enum(Object.keys(emailTemplates) as EmailTemplateName[]),
+      data: z.object(),
     })
-    .safeParse(input);
+    .parse(input);
 
-  if (!baseResult.success)
-    return {
-      success: false,
-      error: baseResult.error,
-    };
+  const { to, template, data } = parsedData;
 
-  const { to, template, data } = baseResult.data;
-
-  const dataResult = emailTemplateDataSchemas[template].safeParse(data);
-
-  if (!dataResult.success)
-    return {
-      success: false,
-      error: dataResult.error,
-    };
+  const dataResult = emailTemplateDataSchemas[template].parse(parsedData);
 
   return {
-    success: true,
-    data: {
-      to: to,
-      template: template,
-      data: dataResult.data,
-    },
+    to: to,
+    template: template,
+    data: dataResult,
   };
 }
 

manmohit-24/just-say

apps/worker/src/emailWorker.ts
@@ -1,9 +1,13 @@
-import { EMAIL_QUEUE_NAME, parseEmailJobData } from "@repo/jobs/email";
-import { Job, Worker } from "bullmq";
-import { sendEmail } from "./mail/mail.js";
-import { env } from "./config/env.js";
-import { logger } from "./shared/logger.js";
-import { JobLogger } from "./shared/jobLogger.js";
+import { Job, UnrecoverableError, Worker } from "bullmq";
+
+import { EMAIL_QUEUE_NAME } from "@repo/jobs/email";
+
+import { env } from "@/config/env.js";
+import { logger } from "@/shared/logger.js";
+import { JobLogger } from "@/shared/jobLogger.js";
+import { mapBullMqError } from "@/shared/errors/mappers/bullMQ.js";
+
+import { sendEmail, validateEmailJobData } from "@/mail/index.js";
 
 const emailJob = async (job: Job) => {
   const jobLogger = new JobLogger({
@@ -14,21 +18,18 @@ const emailJob = async (job: Job) => {
 
   job.jobLogger = jobLogger;
 
-  const response = parseEmailJobData(job.data);
+  try {
+    const data = validateEmailJobData(job.data);
 
-  if (!response.success) {
-    logger.error(response.error);
-    return;
+    jobLogger.options.info = {
+      to: data.to,
+      template: data.template,
+    };
+
+    await sendEmail(data);
+  } catch (error) {
+    throw mapBullMqError(error);
   }
-
-  const data = response.data;
-
-  jobLogger.options.info = {
-    to: data.to,
-    template: data.template,
-  };
-
-  await sendEmail(data);
 };
 
 const emailWorker = new Worker(EMAIL_QUEUE_NAME, emailJob, {
@@ -42,8 +43,19 @@ emailWorker.on("completed", (job) => {
 });
 
 emailWorker.on("failed", (job, err) => {
-  if (!job) logger.error({ err }, "Job failed but no job instance was available");
-  else job.jobLogger.fail(err, { attempt: job.attemptsMade });
+  if (!job) {
+    logger.error({ err }, "Job failed but no job instance was available");
+    return;
+  }
+
+  if (err instanceof UnrecoverableError) {
+    job.jobLogger.fail(err); // no need of attempts or retries
+    return;
+  }
+
+  job.jobLogger.fail(err, {
+    attempt: job.attemptsMade,
+  });
 });
 
 export { emailWorker };

packages/jobs/src/email/templateData.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 import { emailTemplates, type EmailTemplateName } from "./templates.js";
 
-const emailTemplateDataSchemas: Record<EmailTemplateName, z._ZodType> = {
+const emailTemplateDataSchemas: Record<EmailTemplateName, z.ZodObject<z.ZodRawShape>> = {
   welcome: z.object({
     name: z.string(),
     dashboardLink: z.url(),
@@ -77,48 +77,23 @@ type EmailJobData<T extends EmailTemplateName> = {
   data: EmailTemplateData[T];
 };
 
-type EmailJobParseResult =
-  | {
-      success: true;
-      data: EmailJobData<EmailTemplateName>;
-    }
-  | {
-      success: false;
-      error: z.ZodError;
-    };
-
-function parseEmailJobData(input: unknown): EmailJobParseResult {
-  const baseResult = z
+function parseEmailJobData(input: unknown): EmailJobData<EmailTemplateName> {
+  const parsedData = z
     .object({
       to: z.email(),
-      template: z.enum(Object.keys(emailTemplates) as [EmailTemplateName, ...EmailTemplateName[]]),
+      template: z.enum(Object.keys(emailTemplates) as EmailTemplateName[]),
       data: z.unknown(),
     })
-    .safeParse(input);
+    .parse(input);
 
-  if (!baseResult.success)
-    return {
-      success: false,
-      error: baseResult.error,
-    };
+  const { to, template, data } = parsedData;
 
-  const { to, template, data } = baseResult.data;
-
-  const dataResult = emailTemplateDataSchemas[template].safeParse(data);
-
-  if (!dataResult.success)
-    return {
-      success: false,
-      error: dataResult.error,
-    };
+  const dataResult = emailTemplateDataSchemas[template].parse(data);
 
   return {
-    success: true,
-    data: {
-      to: to,
-      template: template,
-      data: dataResult.data,
-    },
+    to: to,
+    template: template,
+    data: dataResult,
   };
 }
 